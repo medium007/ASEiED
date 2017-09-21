a64483b9749ba8386468a20ae4af7448abdb672d
medium007 <patryk.milewczyk95@gmail.com>
2017-09-21 22:32:11 +0200

    Added a chart

--- KrolMilewczykSendrowicz_23fb8ada4ccf4bf19a76bb420c10f118/src/main/scala/project2a/Project2A.scala
+++ KrolMilewczykSendrowicz_23fb8ada4ccf4bf19a76bb420c10f118/src/main/scala/project2a/Project2A.scala
@@ -13,17 +13,23 @@ class Project2A {
     .getOrCreate()
 
   def joinData: Unit = {
-	val tab = sparkSession.read.json("src/main/resources/project2a/data.json")
+	val tab = sparkSession.read.json("src/main/resources/project2a/dataOut.json")
         tab.show()
 	tab.createOrReplaceTempView("TempTab")
         val tab2 = sparkSession.sql("SELECT name AS name2, id AS id2, city AS city2, company AS company2 FROM TempTab")
 	val tab3 = tab.join(tab2, tab.col("name").equalTo(tab2.col("name2")) && tab.col("id").notEqual(tab2.col("id2")), "left")
-   	tab3.show()
+   	tab3.show(100)
 	
         tab.createOrReplaceTempView("Employees")
 	val employeesTab = sparkSession.sql("SELECT name, city, company,(COUNT(name)) AS amount FROM Employees GROUP BY city,company,name ORDER BY amount DESC")
-        employeesTab.show()
+        employeesTab.show(100)
 
+        val seq = employeesTab.collect().toSeq
+        val chartTab = for (i <- seq) yield (i(0).toString + "; " + i(1).toString + "; " + i(2).toString, i(3).asInstanceOf[Long])
+
+        val chart = PieChart(chartTab)
+        chart.show()
+        Thread.sleep(10000)
 	  
   }
 }
